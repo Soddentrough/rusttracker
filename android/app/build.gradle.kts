14de3ffea28073499bfcf0759463ee327819e6f6
Soddentrough <m.j.n.rogers@gmail.com>
2026-09-08 11:29:49 +0900

    v0.9.20: Windows multi-channel LPCM, bitstream passthrough, and AV receiver discovery

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rusttracker.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.9.19"
+        versionCode = 20
+        versionName = "0.9.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
